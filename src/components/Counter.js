84e0d46ed96f848b94551f18d684b62a3fc0cf68
dumazert <rebeccadumazert@hotmail.fr>
2020-03-03 15:26:49 +0100

    create timer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const Counter = () => {
   const [counter, setCounter] = useState(1);
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 100);
+  });
 
   const incrementCounter = () => setCounter(prevCounter => prevCounter + 1);
+
   return (
     <div>
       <button onClick={incrementCounter}>Increment : {counter}</button>
+      <div>{time}</div>
     </div>
   );
 };
