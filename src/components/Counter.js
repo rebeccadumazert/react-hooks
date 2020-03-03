0d621178038ebd391bc95168b0c39c4e148c15b5
dumazert <rebeccadumazert@hotmail.fr>
2020-03-03 15:49:28 +0100

    add button play

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,12 +3,22 @@ import React, { useState, useEffect } from 'react';
 export const Counter = () => {
   const [counter, setCounter] = useState(1);
   const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [isPause, setPause] = useState(true);
 
   useEffect(() => {
-    setInterval(() => {
-      setTime(new Date().toLocaleTimeString());
-    }, 100);
-  });
+    if (!isPause) {
+      const interval = setInterval(() => {
+        setTime(new Date().toLocaleTimeString());
+        console.log(interval);
+      }, 100);
+      return () => clearInterval(interval);
+    }
+  }, [isPause]);
+
+  const pauseTime = () => {
+    setPause(prevPause => !prevPause);
+    console.log(isPause);
+  };
 
   const incrementCounter = () => setCounter(prevCounter => prevCounter + 1);
 
@@ -16,6 +26,7 @@ export const Counter = () => {
     <div>
       <button onClick={incrementCounter}>Increment : {counter}</button>
       <div>{time}</div>
+      <button onClick={pauseTime}>Play/Pause</button>
     </div>
   );
 };
