e7c90648cc81a09e07182e70c7f2bf1084ef4fca
dumazert <rebeccadumazert@hotmail.fr>
2020-03-04 10:32:09 +0100

    add components in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Counter } from './components/Counter';
+import { UseToggle } from './components/UseToggle';
+import { UseArray } from './components/UseArray';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      {/* <UseToggle></UseToggle> */}
+      <UseArray></UseArray>
     </div>
   );
 }
