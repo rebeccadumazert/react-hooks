94aa73ab7cb9be96d003d21f3b699a3d0541f14f
dumazert <rebeccadumazert@hotmail.fr>
2020-03-04 10:32:34 +0100

    add some function in Counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 export const Counter = () => {
   const [counter, setCounter] = useState(1);
+  const [text, setText] = useState('hello');
+  const [img, setImg] = useState(
+    'https://img.icons8.com/carbon-copy/100/000000/filled-like.png'
+  );
+
+  //   const [{}] = useState()
+
   const [time, setTime] = useState(new Date().toLocaleTimeString());
   const [isPause, setPause] = useState(true);
 
@@ -22,11 +29,21 @@ export const Counter = () => {
 
   const incrementCounter = () => setCounter(prevCounter => prevCounter + 1);
 
+  const updateAllState = () => {
+    setCounter(prevCounter => prevCounter + 1);
+    setText('bye bye');
+    setImg('https://img.icons8.com/doodle/48/000000/medical-heart.png');
+  };
+
   return (
     <div>
       <button onClick={incrementCounter}>Increment : {counter}</button>
       <div>{time}</div>
       <button onClick={pauseTime}>Play/Pause</button>
+      <p>counter : {counter}</p>
+      <p>{text}</p>
+      <img src={img} alt="" />
+      <button onClick={updateAllState}>CHANGE ALL</button>
     </div>
   );
 };
